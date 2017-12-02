f102674b095b2ff336e32b2eed151201aaa6d084
Subhranil Mukherjee <subhranilmukherjee190@gmail.com>
2017-12-02 19:50:48 +0530

    Reverted back to native malloc and realloc

--- allocator.h
+++ allocator.h
@@ -3,9 +3,15 @@
 
 #include <stdlib.h>
 
+/*
 void* mallocate(size_t size);
 void* reallocate(void *oldmem, size_t size);
 void memfree(void *mem);
 void memfree_all();
+*/
+#define mallocate(x) malloc(x)
+#define reallocate(x,y) realloc(x, y)
+#define memfree(x) free(x)
+#define memfree_all() {}
 
 #endif
