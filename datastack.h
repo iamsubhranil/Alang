9f68c5446a74b0df4ad9f27a6a576e77973d62dc
Subhranil Mukherjee <subhranilmukherjee190@gmail.com>
2017-12-10 23:51:40 +0530

    datastack : dont decr once incr'ed

--- datastack.h
+++ datastack.h
@@ -37,10 +37,10 @@ static int32_t stackLastIncremented = 0;
 #define binpopv() (Data d1, d2; dpopv(d1, env); dpopv(d2, env);)
 
 #define dpeek() dataStack[sp - 1]
-#define dpop(x) {x = dataStack[--sp]; decr();}
-#define dpopv(x, frame) {x = dataStack[--sp]; x = isidentifer(x)?env_get(tstrk(x), &frame.env, 0):x; decr();}
-#define dpopi(x) {x = tint(dataStack[--sp]); decr();}
-#define dpopf(x) {x = tfloat(dataStack[--sp]); decr();}
-#define dpops(x) {x = tstr(dataStack[--sp]); decr();}
-#define dpopsk(x) {x = tstrk(dataStack[--sp]); decr();}
-#define dpopins(x) {x = tins(dataStack[--sp]); decr();}
+#define dpop(x) {x = dataStack[--sp]; }
+#define dpopv(x, frame) {x = dataStack[--sp]; x = isidentifer(x)?env_get(tstrk(x), &frame.env, 0):x; }
+#define dpopi(x) {x = tint(dataStack[--sp]); }
+#define dpopf(x) {x = tfloat(dataStack[--sp]); }
+#define dpops(x) {x = tstr(dataStack[--sp]); }
+#define dpopsk(x) {x = tstrk(dataStack[--sp]); }
+#define dpopins(x) {x = tins(dataStack[--sp]); }
