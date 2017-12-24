fd787fb7b80238ce7e94cbb20672e05f251f35e6
Subhranil Mukherjee <subhranilmukherjee190@gmail.com>
2017-12-25 01:44:41 +0530

    Switched to nan tagging to store values

--- values.c
+++ values.c
@@ -1,34 +1,84 @@
 #include <stdio.h>
-
+#include <stdint.h>
+#include <inttypes.h>
 #include "values.h"
 #include "allocator.h"
-#include "strings.h"
 #include "env.h"
-#include "display.h"
 
 static uint32_t id = 0;
 
-Data new_array(uint32_t size){
-    Data d;
-    ttype(d) = ARR;
-    tarr(d) = (Array *)mallocate(sizeof(Array));
-    arr_elements(tarr(d)) = (Data *)mallocate(sizeof(Data)*size);
-    uint32_t i = 0;
-    while(i < size)
-        arr_elements(tarr(d))[i++] = new_null();
-    return d;
+Data new_array(int32_t size){
+    Array *arr = (Array *)mallocate(sizeof(Array));
+    arr->numElements = size;
+    arr->arr = (Data *)mallocate(sizeof(Data) * size);
+    return ARR | (uintptr_t)arr;
 }
 
-Data new_ins(void *env, uint32_t name){
-    Data d;
-    d.type = INSTANCE;
-    d.pvalue = ins_new();
-    d.pvalue->container_key = name;
-    d.pvalue->env = ienv_new();
-    ((Environment *)d.pvalue->env)->parent = ((Environment *)env)->parent;
-    ((Environment *)d.pvalue->env)->records = ((Environment *)env)->records;
-    d.pvalue->id = id++;
-    d.pvalue->refCount = 0;
-//    printf(debug("[NewIns] Created [%s#%lu]"), str_get(name), (id - 1));
-    return d;
+Data new_ins(void *env, uint32_t key){
+    Instance *ins = ins_new();
+    ins->container_key = key;
+    ins->env = ienv_new();
+    Environment *n = (Environment *)ins->env;
+    Environment *o = (Environment *)env;
+    n->parent = o->parent;
+    n->records = o->records;
+    ins->refCount = 0;
+    ins->id = id++;
+    return INSTANCE| (uintptr_t)ins;
 }
+
+void print_bit(Data d){
+    uint8_t *bits = (uint8_t *)&d;
+    size_t i = sizeof(d);
+    printf("\tBytes : ");
+    while(i > 0)
+        printf("%x", bits[--i]);
+}
+
+void print_type(const char *expected, Data d){
+    printf("\nExpected : %s", expected);
+    printf("\nReceived : ");
+    if(isfloat(d)){
+        printf("Float\tValue : %F", tfloat(d));
+        //printf("\t&QNAN : ");
+        //print_bit(d & QNAN);
+    }
+    else if(isint(d))
+        printf("Integer\tValue : %" PRId32, tint(d));
+    else if(islogical(d))
+        printf("Logical\tValue : %" PRId32, tlogical(d));
+    else if(isstr(d))
+        printf("String\tKey : %" PRId32 "\tValue : %s", tstrk(d), tstr(d));
+    else if(isidentifer(d))
+        printf("Identifier\tKey : %" PRId32 "\tValue : %s", tstrk(d), tstr(d));
+    else if(isnull(d))
+        printf("Null");
+    else if(isnone(d))
+        printf("None");
+    else if(isins(d))
+        printf("Instance");
+    else if(isarray(d)){
+        printf("Array");
+        Array *arr = tarr(d);
+        printf("\tSize : %" PRId32, arr_size(arr));
+    }
+    
+    print_bit(d);
+
+    printf("\n");
+}
+
+/*
+
+int main(){
+    print_type("Float", new_float(9.9878938438));
+    print_type("Int", new_int(9));
+    print_type("Logical", new_logical(1));
+    print_type("Identifier", new_identifer("Iden"));
+    print_type("String", new_str("This is a string!"));
+    print_type("Null", new_null());
+    print_type("None", new_none());
+    print_type("Array(5)", new_array(5));
+    print_type("Instance", new_ins(NULL, 9));
+}
+*/
--- values.h
+++ values.h
@@ -1,21 +1,101 @@
 #pragma once
 
+// From https://github.com/munificent/wren/blob/master/src/vm/wren_value.h
+// at 9661a5b
+
 #include <stdint.h>
-
+//#include "allocator.h"
 #include "strings.h"
-#include "allocator.h"
+#include <string.h>
 
-typedef enum{
-    INT,
-    FLOAT,
-    STRING,
-    LOGICAL,
-    NIL,
-    INSTANCE,
-    IDENTIFIER,
-    ARR,
-    NONE
-} Datatype;
+typedef uint64_t Data;
+
+// A mask that selects the sign bit.
+#define SIGN_BIT ((uint64_t)1 << 63)
+
+// The bits that must be set to indicate a quiet NaN.
+#define QNAN ((uint64_t)0x7ff8000000000000)
+
+#define ttype(value) (value & 0xffff000000000000)
+
+// If the NaN bits are set, it's not a number.
+#define isfloat(value) ((value & QNAN) != QNAN)
+//#define tfloat(value) ((double)value)
+inline double tfloat(Data value){
+    double x;
+    memcpy(&x, &value, sizeof(double));
+    return x;
+}
+// Masking
+// We need to specify various types that Alang already supports and works with
+// It is done by masking bits 48-50
+// 0 11111111111 1[type]--------------------------------------------
+// 0 indicates it is not a pointer
+// All 1 exponent indicates NAN, highest 1 in mantissa indicates QNAN
+// All integer values will be stored in the lower order 32 bits
+// i.e. : IntMask : 0x00000000ffffffff
+#define MASK 0x00000000ffffffff
+// Types :
+// 1 : Integer (int32_t) => 0 [111 1111 1111] [1 001 ---------------- [value]] => 0x7ff9000000000000
+#define INT 0x7ff9000000000000
+#define isint(value) ((value >> 48) == 0x0000000000007ff9)
+#define isnum(value) (isfloat(value) || isint(value))
+#define tint(value) ((int32_t)(value & MASK))
+#define tnum(value) (isfloat(value)?tfloat(value):tint(value))
+// 2 : String (uint32_t) => 0 [111 1111 1111] [1 010 ---------------- [value]] => 0x7ffa000000000000
+#define STRING 0x7ffa000000000000
+#define isstr(value) ((value >> 48) == 0x0000000000007ffa)
+#define tstrk(value) ((uint32_t)(value & MASK))
+#define tstr(value) (str_get(tstrk(value)))
+// 3 : Identifer (uint32_t) => 0 [111 1111 1111] [1 011 ---------------- [value]] => 0x7ffb000000000000
+#define IDENTIFIER 0x7ffb000000000000
+#define isidentifer(value) ((value >> 48) == 0x0000000000007ffb)
+#define tiden(value) ((uint32_t)(value & MASK))
+// 4 : Logical (int32_t) => 0 [111 1111 1111] [1 100 ------------------ [value]] => 0x7ffc000000000000
+#define LOGICAL 0x7ffc000000000000
+#define islogical(value) ((value >> 48) == 0x0000000000007ffc)
+#define tlogical(value) ((uint32_t)((uint32_t)value & MASK))
+// 5 : Null => 0 [111 1111 1111] [1 101 ----------------- [value]] => 0x7ffd000000000000
+#define NIL 0x7ffd000000000000
+#define isnull(value) ((value >> 48) == 0x0000000000007ffd)
+// 6 : None => 0 [111 1111 1111] [1 110 ------------------ [value]] => 0x7ffe000000000000
+#define NONE 0x7ffe000000000000
+#define isnone(value) ((value >> 48) == 0x0000000000007ffe)
+
+
+Data inline new_float(double x){
+    Data d;
+    memcpy(&d, &x, sizeof(Data));
+    return d;
+}
+//#define new_float(value) (value)
+Data inline new_int(int32_t i){
+    return INT|i;
+}
+//#define new_int(value) ((INT | value))
+Data inline new_strk(uint32_t value){
+    return STRING|value;
+}
+//#define new_strk(value) ((STRING | value))
+Data inline new_str(const char *str){
+    return STRING|str_insert(str);
+}
+//#define new_str(value) ((STRING | str_insert(value)))
+Data inline new_identiferk(uint32_t value){
+    return IDENTIFIER|value;
+}
+//#define new_identiferk(value) ((IDENTIFIER | value))
+Data inline new_identifer(const char *value){
+    return IDENTIFIER|str_insert(value);
+}
+//#define new_identifer(value) ((IDENTIFIER | str_insert(value)))
+Data inline new_logical(int32_t value){
+    return LOGICAL|value;
+}
+//#define new_logical(value) ((LOGICAL | value))
+#define new_null() (NIL)
+#define new_none() (NONE)
+
 
 typedef struct{
     uint32_t container_key;
@@ -24,25 +104,19 @@ typedef struct{
     void *env;
 } Instance;
 
-typedef struct Dat Data;
-
 typedef struct{
+    Data *arr;
     int32_t numElements;
-    Data *data;
 } Array;
 
-struct Dat{
-    Datatype type;
-    //void *env;
-    union{
-        Array *arr;
-        Instance *pvalue;
-        double cvalue;
-        int32_t ivalue;
-        uint32_t svalue;
-    };
-};
-
+// Two types of pointers are used : Instances and Arrays
+// An object pointer is a NaN with a set sign bit.
+#define is_pointer(value) (((value) & (QNAN | SIGN_BIT)) == (QNAN | SIGN_BIT))
+// 1. Instance : 1 [111 1111 1111] 1 100 -------------- => 0xfffc000000000000
+#define INSTANCE 0xfffc000000000000
+#define isins(value) ((value >> 48) == 0x000000000000fffc)
+#define tins(value) ((Instance *)((uintptr_t)value & ~INSTANCE))
+#define tenv(value) ((Environment *)tins(value)->env)
 #define MAX_FREE_INSTANCES 512
 
 extern Instance *freeInstances[MAX_FREE_INSTANCES];
@@ -54,42 +128,13 @@ extern uint16_t freeInstancePointer;
                     else \
                          memfree(x);\
                     }
+// 2. Array : 1 [111 1111 1111] 1 000 ---------------- => 0xfff8000000000000
+#define ARR 0xfff8000000000000
+#define isarray(value) ((value >> 48) == 0x000000000000fff8)
+#define tarr(value) ((Array *)((uintptr_t)value & ~ARR))
+#define arr_size(value) (value->numElements)
+#define arr_elements(value) (value->arr)
 
-#define isint(x) (x.type == INT)
-#define isfloat(x) (x.type == FLOAT)
-#define isnum(x) (isint(x) || isfloat(x))
-#define isstr(x) (x.type == STRING)
-#define isins(x) (x.type == INSTANCE)
-#define islogical(x) (x.type == LOGICAL)
-#define isnull(x) (x.type == NIL)
-#define isidentifer(x) (x.type == IDENTIFIER)
-#define isarray(x) (x.type == ARR)
-#define isnone(x) (x.type == NONE)
+Data new_array(int32_t size);
 
-#define ttype(x) (x.type)
-#define tint(x) (x.ivalue)
-#define tfloat(x) (x.cvalue)
-#define tnum(x) (isint(x)?tint(x):tfloat(x))
-#define tstr(x) (str_get(x.svalue))
-#define tstrk(x) (x.svalue)
-#define tins(x) (x.pvalue)
-#define tenv(x) ((Environment *)x.pvalue->env)
-#define tarr(x) (x.arr)
-#define arr_size(x) (x->numElements)
-#define arr_elements(x) (x->data)
-
-#define new_data() ((Data *)mallocate(sizeof(Data)))
-#define new_int(x) ((Data){.type = INT, {.ivalue = x}})
-#define new_float(x) ((Data){.type = FLOAT, {.cvalue = x}})
-#define new_str(x) ((Data){.type = STRING, {.svalue = str_insert(x)}})
-#define new_strk(x) ((Data){.type = STRING, {.svalue = x}})
-#define new_identifer(x) ((Data){.type = IDENTIFIER, {.svalue = str_insert(x)}})
-#define new_identiferk(x) ((Data){.type = IDENTIFIER, {.svalue = x}})
-#define new_logical(x) ((Data){.type = LOGICAL, {.ivalue = x}})
-#define new_null() ((Data){.type = NIL})
-#define new_none() ((Data){.type = NONE})
-
-Data new_array(uint32_t size);
-Data new_ins(void *env, uint32_t name);
-
-//static inline void data_free(Data d);
+Data new_ins(void *env, uint32_t container_key);
